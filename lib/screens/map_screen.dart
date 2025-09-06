1484e407274e28b564535c4f7b6c73228f4e557d
Jaydip614 <210130107115@gecg28.ac.in>
2025-09-07 04:00:20 +0530

    Add user current location

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -1,5 +1,10 @@
+import 'dart:async';
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:google_maps_in_flutter/utils/constants/locations.dart';
+import 'package:location/location.dart';
 
 class MapScreen extends StatefulWidget {
   const MapScreen({super.key});
@@ -9,44 +14,109 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
-  static const LatLng _pIndiaMap = LatLng(
-    20.593684,
-    78.96288,
-  ); //Location(Wadgaon, Maharashtra)
-  static const LatLng _pGandhinagarMap = LatLng(
-    23.237560,
-    72.647781,
-  ); //Location(Gandhinagar, Gujarat)
-  static const LatLng _pAhemdabadMap = LatLng(
-    23.237560,
-    72.647781,
-  ); //Location(Gandhinagar, Gujarat)
-  static const LatLng _pGandhinagarBusStandMap = LatLng(23.21826, 72.642592);
-  static const LatLng _pGecGandhinagarMap = LatLng(23.25912, 72.653756);
+  ///Location Controller
+  final Location _locationController = Location();
 
-  static const CameraPosition _kIndiaMap = CameraPosition(
-    target: _pGandhinagarMap,
+  /// Map Controller
+  final Completer<GoogleMapController> _mapController =
+      Completer<GoogleMapController>();
+
+  /// User Current Location
+  LatLng? _currentLocation;
+
+  /// Initial Camera Position
+  static const CameraPosition _initCameraPosition = CameraPosition(
+    target: MyLocations.gandhinagarLocation,
     zoom: 13,
   );
+
+  /// Source and Destination Location
+  static const LatLng _sourceLocation = LatLng(23.21826, 72.642592);
+  static const LatLng _destinationLocation = LatLng(23.25912, 72.653756);
+
+  @override
+  void initState() {
+    super.initState();
+    getLocationUpdates();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GoogleMap(
-        initialCameraPosition: _kIndiaMap,
-        markers: {
-          Marker(
-            markerId: MarkerId("_currentLocation"),
-            icon: BitmapDescriptor.defaultMarker,
-            position: _pGandhinagarBusStandMap,
-          ),
-          Marker(
-            markerId: MarkerId("_sourceLocation"),
-            icon: BitmapDescriptor.defaultMarker,
-            position: _pGecGandhinagarMap,
-          ),
-        },
-      ),
+      body: _currentLocation == null
+          ? const Center(child: Text("Loading User Location..."))
+          : GoogleMap(
+              initialCameraPosition: _initCameraPosition,
+              onMapCreated: (GoogleMapController controller) {
+                _mapController.complete(controller);
+              },
+              markers: {
+                Marker(
+                  markerId: MarkerId("_currentLocation"),
+                  icon: BitmapDescriptor.defaultMarker,
+                  position: _currentLocation!,
+                ),
+                Marker(
+                  markerId: MarkerId("_sourceLocation"),
+                  icon: BitmapDescriptor.defaultMarker,
+                  position: _sourceLocation,
+                ),
+                Marker(
+                  markerId: MarkerId("_destinationLocation"),
+                  icon: BitmapDescriptor.defaultMarker,
+                  position: _destinationLocation,
+                ),
+              },
+            ),
     );
+  }
+
+  /// Get Current Location of User
+  Future<void> getLocationUpdates() async {
+    bool isServiceEnabled;
+    PermissionStatus permissionStatus;
+
+    /// Check for GPS Service
+    isServiceEnabled = await _locationController.serviceEnabled();
+    if (isServiceEnabled) {
+      isServiceEnabled = await _locationController.requestService();
+    } else {
+      return; // if service is not enabled
+    }
+
+    /// Check for location Access
+    permissionStatus = await _locationController.hasPermission();
+    if (permissionStatus == PermissionStatus.denied) {
+      permissionStatus = await _locationController.requestPermission();
+    }
+    if (permissionStatus != PermissionStatus.granted) {
+      return; // if permission is not granted
+    }
+
+    _locationController.onLocationChanged.listen((
+      LocationData currentLocation,
+    ) {
+      if (currentLocation.latitude != null &&
+          currentLocation.longitude != null) {
+        setState(() {
+          _currentLocation = LatLng(
+            currentLocation.latitude!,
+            currentLocation.longitude!,
+          );
+          log("User Current Location : $_currentLocation");
+
+          /// Focus on User Location
+          _cameraToPosition(_currentLocation!);
+        });
+      }
+    });
+  }
+
+  /// Animate camera to user position
+  Future<void> _cameraToPosition(LatLng position) async {
+    final GoogleMapController controller = await _mapController.future;
+    CameraPosition newCameraPosition = CameraPosition(target: position, zoom: 13);
+    
+    controller.animateCamera(CameraUpdate.newCameraPosition(newCameraPosition));
   }
 }
---
+++ lib/utils/constants/locations.dart
@@ -0,0 +1,21 @@
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+/// List of Locations
+class MyLocations {
+  MyLocations._(); //private
+
+  static const LatLng pIndiaMap = LatLng(
+    20.593684,
+    78.96288,
+  ); //Location(Wadgaon, Maharashtra)
+  static const LatLng gandhinagarLocation = LatLng(
+    23.237560,
+    72.647781,
+  ); //Location(Gandhinagar, Gujarat)
+  static const LatLng pAhemdabadMap = LatLng(
+    23.237560,
+    72.647781,
+  ); //Location(Gandhinagar, Gujarat)
+  static const LatLng pGandhinagarBusStandMap = LatLng(23.21826, 72.642592);
+  static const LatLng pGecGandhinagarMap = LatLng(23.25912, 72.653756);
+}
